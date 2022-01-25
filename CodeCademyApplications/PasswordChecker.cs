4280a5a718fd10f1154db121535b836968c91733
De Wet van der Merwe <dewetvandermerwe@gmail.com>
2022-01-25 16:49:48 +0200

    Added Routing and Moved Files

---
+++ CodeCademyApplications/PasswordChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingIntoCAgain.CodeCademyApplications
+{
+    public class PasswordChecker
+    {
+        public static void CallMe()
+        {
+            #region
+            string password = "PassWord01";
+            bool doesItHaveUpper;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                doesItHaveUpper = Char.IsUpper(password, i);
+                Console.WriteLine(doesItHaveUpper);
+            }
+            #endregion
+
+            #region
+            //int minLength;
+            //string uppercase;
+            //string lowercase;
+            //string digits;
+            //string specialChars;
+            //int score = 0;
+            //Console.WriteLine("Please enter your password:\n");
+            //string password = Console.ReadLine();
+            //if (password >= minLength)
+            //{
+            //    score += 1;
+            //}
+            #endregion
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,19 +2,11 @@
 
 namespace GettingIntoCAgain
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
 
-            string password = "PassWord01";
-            bool doesItHaveUpper;
-
-            for (int i = 0; i < password.Length; i++)
-            {
-                doesItHaveUpper = Char.IsUpper(password, i);
-                Console.WriteLine(doesItHaveUpper);
-            }
         }
     }
 }
