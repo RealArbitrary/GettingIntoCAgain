9aff8fd1793ea8772b0cd978ce27aee43a592ee8
De Wet van der Merwe <49815767+RealArbitrary@users.noreply.github.com>
2022-02-04 05:55:02 +0200

    Added to MethodArray
    Mainly added "notes" for future reference. Finished the arrays module on codecademy and wanted to remember some of the things I learned seeing as it felt a little more complex than usual.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ namespace GettingIntoCAgain
     {
         static void Main(string[] args)
         {
-            ExquisiteCorpse.CallMe();
-
             var isProgramRunning = false;
             var userDecision = "";
             //TODO add all classes to main menu
@@ -40,13 +38,43 @@ namespace GettingIntoCAgain
                 }
             }
         }
-
-        //For later use
-        static string[] MethodArray()
+        #region
+        //Using an Array method, find the position for the first 3-star rated song and save it to a variable.Print a message to the console, like “Song number X is rated three stars”.
+        private static void MethodArray()
         {
-            string[] myMethods = new string[] {  };
-            return myMethods;
+            string[] summerStrut = new string[] { "Juice", "Missing U", "Raspberry Beret", "New York Groove", "Make Me Feel", "Rebel Rebel", "Despacito", "Los Angeles" };
+
+            int[] ratings = { 5, 4, 4, 3, 3, 5, 5, 4 };
+
+            int myIndex = Array.IndexOf(ratings, 4) + 1;
+
+            Console.WriteLine($"Song Number {myIndex} is rated {myIndex} stars");
+
+            //Demonstrating usage of a predicate
+            var myVar = Array.Find(summerStrut, strut => strut.Length > 10);
+
+            //Organize the playlist alphabetically. To check that it worked, print the first and last song titles to the console.
+
+            Array.Sort(summerStrut);
+
+            foreach (var strut in summerStrut)
+            {
+                Console.WriteLine(strut);
+
+            }
+
+            Console.WriteLine($"First: {summerStrut[0]}, last: {summerStrut[summerStrut.Length - 1]}");
+
+            //find the method that allows you to copy your playlist to a new playlist called summerStrutCopy. Print the first value of summerStrutCopy to the playlist to see if is the same as summerStrut.
+
+            string[] summerStrutCopy = new string[8];            
+
+            Array.Copy(summerStrut, summerStrutCopy, 8);
+
+            Console.WriteLine(summerStrutCopy[0]);
+            Array.Clear(summerStrut, 0, 8);
         }
-        
+        #endregion
+
     }
 }
